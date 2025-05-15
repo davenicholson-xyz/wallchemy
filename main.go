8eb3a2f74254899357fcc31b4f3bcd69409737a2
Dave Nicholson <me@davenicholson.xyz>
2025-05-15 17:29:13 +0100

    cleanup of flag descriptions

--- main.go
+++ main.go
@@ -39,40 +39,38 @@ func runApp() (string, error) {
 
 	flg := config.NewFlagSet()
 
-	flg.DefineString("config", "", "path to config file")
-	flg.DefineString("provider", "wallhaven", "wallpaper provider")
+	flg.DefineString("config", "", "Path to config file")
+	flg.DefineString("provider", "wallhaven", "Wallpaper provider [wallhaven]")
 
 	flg.DefineString("username", "", "wallhaven.cc username")
 	flg.DefineString("apikey", "", "wallhaven.cc api key")
 	flg.DefineBool("nsfw", false, "Fetch NSFW images")
-	flg.DefineBool("noanime", false, "dont fetch anima wallpaper")
-	flg.DefineString("seed", "", "random seed for search")
-	flg.DefineInt("expiry", 0, "cache expiry in seconds")
+	flg.DefineBool("noanime", false, "Dont fetch anima wallpaper")
+	flg.DefineString("seed", "", "Random seed for search")
+	flg.DefineInt("expiry", 0, "Cache expiry in seconds")
 
-	flg.DefineString("collection", "", "collection to select from")
-	flg.DefineString("random", "", "query for random wallpaper")
-	flg.DefineBool("hot", false, "hot")
-	flg.DefineBool("top", false, "toplist")
-	flg.DefineString("id", "", "fetch wallpaper with ID")
+	flg.DefineString("collection", "", "Users collection to select from")
+	flg.DefineString("random", "", "Query for random wallpaper")
+	flg.DefineBool("hot", false, "Select random wallpaper from the hot list")
+	flg.DefineBool("top", false, "Select random wallpaper from the toplist")
+	flg.DefineString("id", "", "Fetch wallpaper with ID")
 
-	flg.DefineString("script", "", "script to set wallpaper")
-	flg.DefineBool("stealth", false, "do not change wallpaper")
-	flg.DefineBool("silent", false, "does not output the filename")
-	flg.DefineBool("file", false, "output the filename of the current wallpaper")
-	flg.DefineBool("url", false, "output the url of the current wallpaper")
-	flg.DefineBool("link", false, "output the provider link of the current wallpaper")
+	flg.DefineString("script", "", "Custom script to set wallpaper")
+	flg.DefineBool("stealth", false, "Fetch but do not set wallpaper")
+	flg.DefineBool("silent", false, "Do not output filename after fetch")
+	flg.DefineBool("file", false, "Output the filename of the current wallpaper")
+	flg.DefineBool("url", false, "Output the url of the current wallpaper")
+	flg.DefineBool("link", false, "Output the provider link of the current wallpaper")
 
-	flg.DefineBool("clear", false, "clear the wallchemy cache")
+	flg.DefineBool("clear", false, "Clear the wallchemy cache")
 
-	flg.DefineBool("version", false, "version")
-	flg.DefineBool("v", false, "version")
+	flg.DefineBool("version", false, "Version info")
 
 	flgValues := flg.Collect()
 
 	_, version_ok := flgValues["version"]
-	_, v_ok := flgValues["v"]
 
-	if version_ok || v_ok {
+	if version_ok {
 		return version, nil
 	}
 
